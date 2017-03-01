6f7ee29108518f8e78cbf0b8fc6188f3bdd1dd7a
Kamil <kamil.rajca@gmail.com>
2017-03-01 19:02:34 +0100

    stworzenei projektu

---
+++ gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'build/css/style.css': 'sass/main.scss'
+        }
+      }
+    },
+    // optymalizacja obrazkow
+    // imagemin: {
+    //   dynamic: {
+    //     files: [{
+    //       expand: true,
+    //       cwd: 'images/',
+    //       src: ['**/*.{png,jpg,gif}'],
+    //       dest: 'build/images/'
+    //     }]
+    //   }
+    // },
+
+    watch: {
+      scripts: {
+          files: ['sass/*.scss'],
+          tasks: ['sass'],
+          options: {
+              spawn: false,
+          },
+      } 
+    }
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  // grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  
+  grunt.registerTask('default', ['sass', 'watch']);
+};
